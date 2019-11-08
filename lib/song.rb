6072358da4078c7340022ecb4d53b4a73d868f8a
Brandon Weaver <a7xncob@gmail.com>
2019-11-08 19:46:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,9 +9,9 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
-    self.count += 1
-    self.artists << artist if !self.artists.include?(artist)
-    self.genres << genre
+    @@count += 1
+    @@artists << artist if !@@artists.include?(artist)
+    @@genres << genre
   end
   
   def self.count
