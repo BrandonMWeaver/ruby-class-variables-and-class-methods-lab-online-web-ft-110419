098214129e893e6351010c45f6c83331286e64e8
Brandon Weaver <a7xncob@gmail.com>
2019-11-08 19:33:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,9 @@
 class Song
-  attr_reader :name
+  attr_reader :name, :artist, :genre
   
   def initialize(name, artist, genre)
     self.name = name
+    self.artist = artist
+    self.genre = genre
   end
 end
